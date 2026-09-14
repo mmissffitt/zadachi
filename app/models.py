1615c60ffc7f6fafd0df57817c1e1ddce05055df
Студент группы IS <is@sielom.local>
2026-09-14 12:54:48 +0500

    модели

--- app/models.py
+++ app/models.py
@@ -3,8 +3,24 @@ from django.db import models
 class Tags(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return f"{self.id}: ({self.name})"
+
+
+
 class Tasks(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tags, blank=True)
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return f"{self.id}: {self.title}"
